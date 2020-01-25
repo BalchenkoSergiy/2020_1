b251921c072c5b65bac11f3a856af88fd797e296
BalchenkoSergiy <sergiybalchenko@gmail.com>
2020-01-25 22:32:05 +0200

    Modified st_dictionaries.py

--- st_dictionaries.py
+++ st_dictionaries.py
@@ -3,64 +3,83 @@
 # *****Lesson 23 'Dictionaries'
 
 d = {}
-product1 = {'title': 'Sony', 'price': 100}
-product2 = dict(title='iPhone', price=110)
+print('1-->', type(d))# <class 'dict'>
 
-# **********************************************************************************************************************
+print('***************************************************************************************************************')
+product1 = {'title': 'Sony',
+            'price': 100}
+product2 = dict(title='iPhone',
+                price=110)
+
+print('2-->', product1)# {'title': 'Sony', 'price': 100}
+print('3-->' ,product2)# {'title': 'iPhone', 'price': 110}
+
+print('***************************************************************************************************************')
+#Создаем словарь из списка или кортежа
 users = [
      ['bob@gmail.com', 'Bob'],
      ['katy@gmail.com', 'Katy'],
      ['john@gmail.com', 'John']
  ]
 d_users = dict(users)
-print(users)
-print(d_users)
+print('4-->', users)# [['bob@gmail.com', 'Bob'], ['katy@gmail.com', 'Katy'], ['john@gmail.com', 'John']]
+print('5-->', d_users)# {'bob@gmail.com': 'Bob', 'katy@gmail.com': 'Katy', 'john@gmail.com': 'John'}
 
-# **********************************************************************************************************************
+print('***************************************************************************************************************')
 users_t = (
      ('bob@gmail.com', 'Bob'),
      ('katy@gmail.com', 'Katy'),
      ('john@gmail.com', 'John')
 )
 t_users = dict(users_t)
-print(users_t)
-print(t_users)
+print('6-->', users_t)# (('bob@gmail.com', 'Bob'), ('katy@gmail.com', 'Katy'), ('john@gmail.com', 'John'))
+print('7-->', t_users)# {'bob@gmail.com': 'Bob', 'katy@gmail.com': 'Katy', 'john@gmail.com': 'John'}
 
-# **********************************************************************************************************************
-# print(d)
-# print(product1)
-# print(product2)
+print('***************************************************************************************************************')
+product3 = dict.fromkeys(['price1', 'price2', 'price3'], 50)
+print('8-->', product3)# 8--> {'price1': 50, 'price2': 50, 'price3': 50}
 
-# product3 = dict.fromkeys(['price1', 'price2', 'price3'], 50)
-# print(product3)
+print('***************************************************************************************************************')
+# Используем генератор для создания словаря
+nums = {i: i + 1 for i in range(1, 10)}
+print('9-->', nums)# 9--> {1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8, 8: 9, 9: 10}
 
-# nums = {i: i + 1 for i in range(1, 10)}
-# print(nums)
+print('***************************************************************************************************************')
+#print(nums['1']) # error
+print('10-->', nums[1]) # 2
 
+print('***************************************************************************************************************')
+# Выводим значение по ключу, но обходим ошибки
 product1 = {'title': 'Sony', 'price': 100}
 product2 = dict(title='iPhone', price=110)
 
-# print(product1['title2'])
-# print(product1.get('title2', 'NO')) # if no key
+print('11-->', product1['price'])# 100
+print('12-->', product1.get('price2', 'NO')) # if no key
 
-# print(nums['1']) # error
-# print(nums['1']) # OK
+print('***************************************************************************************************************')
+# перебираем словарь
+for key in product1:
+    print('13-->', f'{key}: {product1[key]}')   # title: Sony
+                                                # price: 100
 
-# for key in product1:
-#     print(f'{key}: {product1[key]}')
-#
-#
-# for key, value in product1.items():
-#     print(key, value)
+print('***************************************************************************************************************')
+# Возвращаем пары "Ключь - Значения"
+for key, value in product1.items():
+    print('14-->', key, value)  # title Sony
+                                # price 100
 
-# products = [
-#     {'title': 'Sony', 'price': 100},
-#     {'title': 'iPhone', 'price': 110},
-#     {'title': 'Samsung', 'price': 90}
-# ]
-#
-# print(products)
-#
-# for product in products:
-#     print(product['title'], product['price'])
+print('***************************************************************************************************************')
+# Используем список словарей
+products = [
+    {'title': 'Sony', 'price': 100},
+    {'title': 'iPhone', 'price': 110},
+    {'title': 'Samsung', 'price': 90}
+]
+print('15-->', products) # [{'title': 'Sony', 'price': 100}, {'title': 'iPhone', 'price': 110}, {'title': 'Samsung', 'price': 90}]
+
+print('***************************************************************************************************************')
+for product in products:
+    print('16-->', product['title'], product['price'])  # Sony 100
+                                                        # iPhone 110
+                                                        # Samsung 90
 
