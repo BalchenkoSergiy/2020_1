ba0e97eeb99c019c886699c86297497bf5cc7473
BalchenkoSergiy <sergiybalchenko@gmail.com>
2020-03-28 22:23:12 +0200

    added temporary image for future deleting and created new lesson_4

---
+++ st_parsing_34.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+req = urllib.request.urlopen('https://www.ua-football.com/sport')
+
+html = req.read()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+news = soup.find_all('li', class_='liga-news-item')
+
+result = []
