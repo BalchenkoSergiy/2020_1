f89b30c3a3dc0e3553ff5495999361920d735e6c
BalchenkoSergiy <sergiybalchenko@gmail.com>
2020-03-29 15:58:08 +0300

    Modified st_parsin_34.py

--- st_parsing_34.py
+++ st_parsing_34.py
@@ -9,4 +9,21 @@ soup = BeautifulSoup(html, 'html.parser')
 
 news = soup.find_all('li', class_='liga-news-item')
 
-result = []
+results = []
+
+for item in news:
+    title = item.find('span', class_='d-block').get_text(strip=True)
+    desc = item.find('span', class_='name-dop').get_text(strip=True)
+    href = item.a.get('href')
+    results.append({
+        'title': title,
+        'desc': desc,
+        'href': href,
+    })
+
+f = open('news.txt', 'w', encoding='utf-8')
+i = 1
+for item in results:
+    f.write(f'Новость №{i}\n\nНазвание: {item["title"]}\nОписание: {item["desc"]}\nСсылка: {item["href"]}\n\n***********\n')
+    i += 1
+f.close()
