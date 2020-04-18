ea8355866a1afdfb3c3a788f58e64bbeaff9ebf9
BalchenkoSergiy <sergiybalchenko@gmail.com>
2020-04-18 13:18:23 +0300

    Modified st_45_SQLite file

--- st_SQLite/st_45_SQLite_part_One.py
+++ st_SQLite/st_45_SQLite_part_One.py
@@ -1,14 +1,52 @@
+# Імпортую модуль sqlite3
 import sqlite3
 
+# Створюю БД (якщо вона вже є, то я конекчусь до неї)
 db = sqlite3.connect('my_db.db')
+# Створюю cursor для подальших запитів до нашої БД
 cursor = db.cursor()
 
-cursor.execute('''
-    CREATE TABLE users (
-        id INTEGER PRIMARY KEY, 
-        name TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE)
-    ''')
+# Створюю таблицю users
+#cursor.execute('''
+#    CREATE TABLE users (
+#        id INTEGER PRIMARY KEY,
+#        name TEXT NOT NULL,
+#        email TEXT NOT NULL UNIQUE)
+#    ''')
 
+# Вставляю данні у таблицю users
+#cursor.execute('''
+#    INSERT INTO users (name, email)
+#        VALUES
+#            ('Petrov Petr', 'petrov@com'),
+#            ('Sidorov Oleg', 'sidorov@com')
+#    ''')
+#db.commit()
+
+# Якщо мені потрібно створити два запити в одному кейсі то я використовую executescript
+#cursor.executescript('''
+#    INSERT INTO users (name, email)
+#        VALUES
+#            ('Stallone Silvester', 'stallone@com');
+#    INSERT INTO users (name, email)
+#        VALUES
+#            ('Lee Brandon', 'lee@com');
+#    ''')
+#db.commit()
+
+# Створюю список кортежей із юзерами для подальшого використання методу executemany()
+users = [
+    ('User_1', 'user_1@gmail.com'),
+    ('User_2', 'user_2@gmail.com'),
+    ('User_3', 'user_3@gmail.com')
+]
+
+# Тепер із-за допомогою методу executemany() я вставляю у таблицю одразу цілу колекцію
+#cursor.executemany('''
+#    INSERT INTO users (name, email)
+#        VALUES
+#            (?, ?)''', users
+#    )
+#db.commit()
 
 db.close()
