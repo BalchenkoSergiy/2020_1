ecbcffdc71c5d2845ea3e97a8e6dd02cc9f0c3a4
BalchenkoSergiy <sergiybalchenko@gmail.com>
2020-05-17 14:52:09 +0300

    Finished my easy calc (Will be updating)

--- tk_test_entry/classes.py
+++ tk_test_entry/classes.py
@@ -2,8 +2,11 @@ class Calc:
     result = 0
 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        try:
+            self.x = int(x)
+            self.y = int(y)
+        except:
+            None
 
     def plus(self):
         global result
--- tk_test_entry/entry_test_file.py
+++ tk_test_entry/entry_test_file.py
@@ -2,22 +2,50 @@ from tkinter import *
 from tk_test_entry import classes
 
 root = Tk()
+root.title('My easy calc')
 root.geometry('650x200+1000+300')
 root['bg'] = 'grey'
+
 # Створюю фрейм one
 frame_one = Frame(root, bg='grey')
 frame_one.pack()
 
 total_result = None
+param = None
 
 # Створюю функцію яка буде брати значення введених чисел і передавати їх у клас
-def get_first():
+def logic():
     global total_result
     total_result = classes.Calc(e_first_num.get(), e_sec_num.get())
-    l_result['text'] = f'First number: {e_first_num.get()}; Second number: {e_sec_num.get()};'
+    if param == '+':
+        result = total_result.plus()
+    if param == '-':
+        result = total_result.minus()
+    if param == '/':
+        result = total_result.divided()
+    if param == '*':
+        result = total_result.multiply()
+    l_result['text'] = f'{e_first_num.get()} {param} {e_sec_num.get()} = {result};'
 
+# Створюю функцію яка встановлює параметр знаку дії
+def set_plus():
+    global param
+    param = '+'
 
+# Створюю функцію яка встановлює параметр знаку дії
+def set_minus():
+    global param
+    param = '-'
 
+# Створюю функцію яка встановлює параметр знаку дії
+def set_divided():
+    global param
+    param = '/'
+
+# Створюю функцію яка встановлює параметр знаку дії
+def set_multiply():
+    global param
+    param = '*'
 
 # Лейбл та ввід першої цифри
 l_first_num = Label(frame_one, text='First number:', bg='grey')
@@ -27,16 +55,16 @@ e_first_num = Entry(frame_one, bd=5)
 e_first_num.pack(side='left', anchor='nw')
 
 # Створюю кнопки
-my_button = Button(frame_one, text='+', bg='green')
+my_button = Button(frame_one, text='+', bg='green', command=set_plus)
 my_button.pack(side=LEFT, anchor='nw', ipadx=4, padx=6)
 
-my_button = Button(frame_one, text='-', bg='green')
+my_button = Button(frame_one, text='-', bg='green', command=set_minus)
 my_button.pack(side=LEFT, anchor='nw', ipadx=5, padx=6)
 
-my_button = Button(frame_one, text='*', bg='green')
+my_button = Button(frame_one, text='*', bg='green', command=set_multiply)
 my_button.pack(side=LEFT, anchor='nw', ipadx=5, padx=6)
 
-my_button = Button(frame_one, text='/', bg='green')
+my_button = Button(frame_one, text='/', bg='green', command=set_divided)
 my_button.pack(side=LEFT, anchor='nw', ipadx=5, padx=6)
 
 
@@ -54,7 +82,7 @@ frame_two.pack()
 
 
 # Створюю кнопку =
-my_button = Button(frame_two, height=2, width=50, text='=', bg='red', command=get_first)
+my_button = Button(frame_two, height=2, width=50, text='=', bg='red', command=logic)
 my_button.pack(pady=20)
 
 
