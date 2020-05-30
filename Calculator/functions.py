7007d6cc3b174c7fb3cdee6f83498172dc1aecea
BalchenkoSergiy <sergiybalchenko@gmail.com>
2020-05-30 13:56:18 +0300

    modified functions

--- Calculator/functions.py
+++ Calculator/functions.py
@@ -1,11 +1,31 @@
 entry_list = []
+symbol = ''
+
 
 def show_inserted(lable, symb, my_list):
-    lable['text'] = f'{create_str(my_list, symb)}'
+    global symbol
+    if symb != '+' and symb != '-' and symb != '/' and symb != '*':
+        lable['text'] = f'{create_str(my_list, symb)}'
+    else:
+        symbol = symb
+    print(my_list)
+    print(symbol)
+
 
 def create_str(my_list, symb):
-    if len(my_list) > 0:
-        my_list[0] = my_list[0] + symb
-    elif len(my_list) == 0:
-        my_list.append(symb)
-    return my_list[0]
+    global symbol
+    if symbol == '':
+        if len(my_list) > 0:
+            my_list[0] = my_list[0] + symb
+        elif len(my_list) == 0:
+            my_list.append(symb)
+        return my_list[0]
+    else:
+        if len(my_list) > 1:
+            my_list[1] = my_list[1] + symb
+        elif len(my_list) == 1:
+            my_list.append(symb)
+        return my_list[1]
+
+
+
