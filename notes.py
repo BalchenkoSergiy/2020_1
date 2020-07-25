8633c04802928a5480b7cf9865b1f1d8d99fc7b5
BalchenkoSergiy <sergiybalchenko@gmail.com>
2020-07-25 22:49:10 +0300

    Testing method Scrollbar

--- notes.py
+++ notes.py
@@ -103,10 +103,52 @@ root.geometry('600x400+1000+300')
 # btn3 = Button(text='Button 1', bg="#f1c40f", fg="#fff", font="16", padx=20, pady=8)
 # btn3.place(relx=1, rely=1, anchor='se')
 
-l = Label(root, bg="#000")
-l.place(relheight=0.8, relwidth=0.8, relx=0.1, rely=0.1)
+#l = Label(root, bg="#000")
+#l.place(relheight=0.8, relwidth=0.8, relx=0.1, rely=0.1)
+#
+#btn2 = Button(text='Button 1', bg="#2ecc71", fg="#fff", font="16", padx=20, pady=8)
+#btn2.place(relx=0.5, rely=0.5, anchor='c')
 
-btn2 = Button(text='Button 1', bg="#2ecc71", fg="#fff", font="16", padx=20, pady=8)
-btn2.place(relx=0.5, rely=0.5, anchor='c')
+#from tkinter import *
+#
+#root = Tk()
+#root.geometry('600x400+1000+300')
+
+f_menu = Frame(root, bg="#1F252A", height=40)
+f_text = Frame(root)
+f_menu.pack(fill=X)
+f_text.pack(fill=BOTH, expand=1)
+
+l_menu = Label(f_menu, text="Menu", bg="#2B3239", fg="#C6DEC1", font="Arial 10")
+l_menu.place(x=10, y=10)
+
+
+def add_str():
+    t.insert('2.4', 'Hello!')
+
+
+def del_str():
+    # t.delete('2.4', '2.10')
+    t.delete('1.0', END)
+
+
+def get_str():
+    print(t.get('1.0', END))
+
+
+btn_add = Button(root, text="Add", command=add_str).place(x=50, y=10)
+btn_del = Button(root, text="Delete", command=del_str).place(x=90, y=10)
+btn_get = Button(root, text="Get", command=get_str).place(x=140, y=10)
+
+t = Text(f_text, bg="#343D46", fg="#C6DEC1", padx=10, pady=10, wrap=WORD, insertbackground="#EDA756", selectbackground="#4E5A65", width=30, spacing3=10)
+t.pack(fill=BOTH, expand=1, side=LEFT)
+
+scroll = Scrollbar(f_text, command=t.yview)
+scroll.pack(fill=Y, side=LEFT)
+t.config(yscrollcommand=scroll.set)
+
+
+root.mainloop()
+
 
 root.mainloop()
